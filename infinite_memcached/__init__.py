afcef3a151dcff1006239c1824970fe53053a390
albsen <albsen@gmail.com>
2012-03-05 11:52:45 +0400

    make_infinite_caching_default: added exception handling to catch all the possible Exceptions coming from pylibmc and channel them into a logger instead of breaking something further up the chain

--- infinite_memcached/__init__.py
+++ infinite_memcached/__init__.py
@@ -1,4 +1,8 @@
 from django.core.cache.backends import memcached
+from django.utils.log import getLogger
+import sys
+
+logger = getLogger('infinite_memcache.cache.backends')
 
 class MemcachedCache(memcached.MemcachedCache):
     def _get_memcache_timeout(self, timeout):
@@ -32,3 +36,23 @@ class PyLibMCInfiniteCache(memcached.PyLibMCCache):
         """Overriding _get_memcache_timeout so that it defaults to infinite"""
         if timeout == 0 or timeout == None: return 0
         else: return super(PyLibMCInfiniteCache, self)._get_memcache_timeout(timeout)
+
+    def get(self, *args, **kwargs):
+        """
+        pyLibMC is quite chatty about it's status - aka breaks the site a lot if memcache has trouble.
+        - this should catch all possible messages and tunnel them into something
+        """
+        try:
+            return super(PyLibMCInfiniteCache, self).get(*args,**kwargs)
+        except Exception, e:
+            logger.debug('PyLibMCInfiniteCache.get() raised: %s' % e, exc_info=sys.exc_info(),extra={})
+
+    def set(self, *args, **kwargs):
+        """
+        pyLibMC is quite chatty about it's status - aka breaks the site a lot if memcache has trouble.
+        - this should catch all possible messages and tunnel them into something
+        """
+        try:
+            super(PyLibMCInfiniteCache, self).set(*args, **kwargs)
+        except Exception, e:
+            logger.debug('PyLibMCInfiniteCache.set() raised: %s' % e, exc_info=sys.exc_info(),extra={})
